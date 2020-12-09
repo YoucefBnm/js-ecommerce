301b1fe2aa58c6cc220ce00a9fed2e82c6810f4d
Josef Bnm <ssefbnm@gmail.com>
2020-12-09 19:21:00 +0100

    add cards

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Product from './Product/Product'
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes', price: '$89.99' },
+    { id: 2, name: 'MacBook', description: 'Apple macbook', price: '$950.00'}
+]
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify='center' spacing={4}>
+                {
+                    products.map((product) => {
+                        return(
+                            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <Product product={product} />
+                        </Grid>   
+                        ) 
+                    })
+                }
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
